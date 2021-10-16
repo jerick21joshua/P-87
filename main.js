e966e33229ece9cb02aa4ed514f14f2a6f3b0d92
jerick21joshua <78532477+jerick21joshua@users.noreply.github.com>
2021-10-16 17:31:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	function new_image(get_image)
+	{
+		fabric.image.fromURL(get_image, function(Img){
+			block_image_object = Img;
+
+			block_image_object.scaleToWidth(block_image_width);
+			block_image_object.scaleTohieght(block_image_height);
+			block_image_object.set({
+				top:block_y,
+				left:block_x,
+			});
+			canvas.add(block_image_object);
+		
+
+			
+		}
+		
+
+		)
+	}
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') 
+    {
+	new_image('rrl.png');
+	console.log("r")	
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		new_image('')
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	// upload blue ranger
+	}
+	
+}
+
